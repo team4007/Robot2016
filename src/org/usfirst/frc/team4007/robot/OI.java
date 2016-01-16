3221a0c96d47ce99f97e388cc68965cc2c07ac33
Team4007 <team4007@collegeshawinigan.ca>
2016-01-16 14:15:00 -0500

    Mathieu L

--- src/org/usfirst/frc/team4007/robot/OI.java
+++ src/org/usfirst/frc/team4007/robot/OI.java
@@ -14,7 +14,8 @@ public class OI {
     // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
+    // gdsgeargreagrgrtgeargrgarg
+	
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
