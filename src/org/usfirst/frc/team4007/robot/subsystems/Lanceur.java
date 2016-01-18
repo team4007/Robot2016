ad1480c93828534f5fb25b25803288768ce754cf
team4007 <team4007collegeshawinigan.ca>
2016-01-18 18:25:07 -0500

    Moteur essieus et fonctions

--- src/org/usfirst/frc/team4007/robot/subsystems/Lanceur.java
+++ src/org/usfirst/frc/team4007/robot/subsystems/Lanceur.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4007.robot.subsystems;
 
+import org.usfirst.frc.team4007.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -14,10 +16,42 @@ public class Lanceur extends Subsystem {
 	public Jaguar essieuMilieu;
 	public Jaguar essieuBas;
 	
+	private double throwingSpeed = 1.0;
+	private double swallowingSpeed = 1.0;
+	
+	
+	public Lanceur(){
+		super();
+		essieuHaut = new Jaguar(RobotMap.PWMEssieuHaut);
+		essieuMilieu = new Jaguar (RobotMap.PWMEssieuMilieu);
+		essieuBas = new Jaguar (RobotMap.PWMEssieuBas);
+		
+	}
+	
+	
+	
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    public void stop(){
+    	essieuHaut.stopMotor();
+    	essieuMilieu.stopMotor();
+    	essieuBas.stopMotor();
+    	
+    }
+
+    public void lancer(){
+    	essieuHaut.set(throwingSpeed);
+    	essieuMilieu.set(-throwingSpeed);
+    }
+    public void gober(){
+    	essieuBas.set(swallowingSpeed);
+    	
+    }
+
 }
+
 
