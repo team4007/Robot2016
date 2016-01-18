56171f8e68e1d950b292670cf05f054ebe5c3320
team4007 <team4007@collegeshawinigan.ca>
2016-01-18 17:57:59 -0500

    Ajout Essieux RobotMap

--- src/org/usfirst/frc/team4007/robot/RobotMap.java
+++ src/org/usfirst/frc/team4007/robot/RobotMap.java
@@ -15,4 +15,7 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	public static int PWMEssieuHaut = 7;
+	public static int PWMEssieuMilieu = 8;
+	public static int PWMEssieuBas = 9;
 }
